4234d6ba14191a92c041f9f1e109ac8018188365
Damien LaRocque <phicoltan@gmail.com>
2020-12-09 00:59:25 -0500

    Start working on a VLC MediaPlayer

---
+++ vlc_media_player.py
@@ -0,0 +1,23 @@
+import pafy
+import vlc
+
+
+class VLCMediaPlayer:
+    """VLCMedia Player
+    Play a video with VLC, from a YouTube url
+    """
+
+    def __init__(self, url="https://www.youtube.com/watch?v=xtp4msMYi9s"):
+        self.url = url
+        video = pafy.new(self.url).getbest()
+        self.media = vlc.MediaPlayer(video.url)
+
+    def play(self):
+        self.media.play()
+
+
+if __name__ == "__main__":
+    media_player = VLCMediaPlayer()
+    media_player.play()
+    while True:
+        pass
