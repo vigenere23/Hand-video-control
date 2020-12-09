16c43e2fd5742cc3cab23623ed231053cb4fb37a
GASTP33 <gabriel.st-pierre.6@ulaval.ca>
2020-12-09 16:15:10 -0500

    100% accuracy daaamn

--- analyzing.py
+++ analyzing.py
@@ -1,18 +1,21 @@
 import cv2
+import torch
+from torch import nn
 from cnn import load_model
 from image import bgr_image_to_tensor
 from matplotlib import pyplot as plt
 import numpy as np
 
 
-def run():
-    model, *_ = load_model('0.984_Net_1607533766.8713942')
-    model.eval()
+def test_realworld_images(model: nn.Module):
+    if torch.cuda.is_available():
+        model = model.cuda()
 
+    model.eval()
     letters = ['A', 'L', 'Q']
 
     for letter in letters:
-        img = cv2.imread(f"{letter}.jpg")
+        img = cv2.imread(f"test_images/{letter}.jpg")
         img = bgr_image_to_tensor(img)
 
         img = img.cuda()
@@ -24,6 +27,11 @@ def run():
             print(f"I'm genius, I know that a {sign} is a {letter}")
         else:
             print(f"I'm dumb, I think that a {letter} is a {sign}")
+
+
+def run():
+    model, *_ = load_model('1.0_Net_1607547806.270809')
+    test_realworld_images(model)
 
 
 if __name__ == "__main__":
--- training.py
+++ training.py
@@ -2,10 +2,12 @@ import pandas as pd
 import numpy as np
 import torch
 from torch import nn, optim
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, random_split
 from torchvision import transforms
 from cnn import Net, GoogleNet, save_model, load_model
 from image import images_to_tensor, creating_images_array
+from analyzing import test_realworld_images
+import cv2
 
 
 def load_csv_dataset(csv_path: str):
@@ -39,16 +41,21 @@ def load_dataset(dataset_type: str):
 class SignLanguageDataset():
     def __init__(self, dataset_type: str):
         self.__images, self.__target = load_dataset(dataset_type)
+        # self.__images = np.repeat(self.__images[..., np.newaxis], 3, -1)
+        # self.__images = np.transpose(self.__images, (0, 1, 4, 2, 3))
+
         self.transform = nn.Sequential(
             transforms.RandomResizedCrop((28, 28), scale=(0.9, 1.1), ratio=(9./10., 10./9.)),
-            transforms.RandomRotation(10)
+            transforms.RandomRotation(10),
+            transforms.RandomHorizontalFlip()
+            # transforms.Resize(224)
         )
 
     def __len__(self):
         return self.__target.shape[0]
 
     def __getitem__(self, index):
-        return self.__images[index], self.__target[index]
+        return self.transform(self.__images[index]), self.__target[index]
 
 
 def calculate_accuracy(output, target):
@@ -57,15 +64,19 @@ def calculate_accuracy(output, target):
     return accuracy
 
 
-def train_epoch(model: nn.Module, optimizer: optim.Optimizer, criterion: nn.Module, train_gen: DataLoader):
+def split_dataset(dataset: Dataset, factor: float = 0.5):
+    first_dataset_length = int(len(dataset) * factor)
+    second_dataset_length = len(dataset) - first_dataset_length
+    return random_split(dataset, [first_dataset_length, second_dataset_length])
+
+
+def train_epoch(model: nn.Module, train_gen: DataLoader, val_gen: DataLoader, optimizer: optim.Optimizer, criterion: nn.Module, scheduler: object = None):
     losses = []
     accuracies = []
     model.train()
 
     for batch, (images, target) in enumerate(train_gen):
         if torch.cuda.is_available():
-            model = model.cuda()
-            criterion = criterion.cuda()
             images = images.cuda()
             target = target.cuda()
 
@@ -73,25 +84,38 @@ def train_epoch(model: nn.Module, optimizer: optim.Optimizer, criterion: nn.Modu
 
         output = model(images)
         loss = criterion(output, target)
-        accuracy = calculate_accuracy(output, target)
 
         loss.backward()
         optimizer.step()
 
-        losses.append(loss.item())
-        accuracies.append(accuracy.item())
+        # print("Batch: ", batch, "\t", "loss: ", loss.item())
 
-        # print("Batch: ", batch, "\t", "loss: ", loss.item(), "\t", "accuracy: ", accuracy.item())
+    with torch.no_grad():
+        for batch, (images, target) in enumerate(val_gen):
+            if torch.cuda.is_available():
+                images = images.cuda()
+                target = target.cuda()
+            
+            output = model(images)
+            loss = criterion(output, target)
+            accuracy = calculate_accuracy(output, target)
+
+            losses.append(loss.item())
+            accuracies.append(accuracy.item())
+
 
     epoch_loss = np.array(losses).mean()
     epoch_accuracy = np.array(accuracies).mean()
 
-    print("TRAIN", "\t", "loss: ", epoch_loss, "\t", "accuracy: ", epoch_accuracy)
+    if scheduler is not None:
+        scheduler.step(epoch_loss)
+
+    print("VAL", "\t", "loss: ", epoch_loss, "\t", "accuracy: ", epoch_accuracy)
 
     return epoch_loss, epoch_accuracy
 
 
-def test_epoch(model: nn.Module, criterion: nn.Module, test_gen: DataLoader):
+def test_epoch(model: nn.Module, test_gen: DataLoader, criterion: nn.Module):
     losses = []
     accuracies = []
     model.eval()
@@ -99,8 +123,6 @@ def test_epoch(model: nn.Module, criterion: nn.Module, test_gen: DataLoader):
     with torch.no_grad():
         for images, target in test_gen:
             if torch.cuda.is_available():
-                model = model.cuda()
-                criterion = criterion.cuda()
                 images = images.cuda()
                 target = target.cuda()
                 
@@ -119,7 +141,7 @@ def test_epoch(model: nn.Module, criterion: nn.Module, test_gen: DataLoader):
     return epoch_loss, epoch_accuracy
 
 
-def train(model: nn.Module, n_epochs: int, batch_size: int, optimizer: optim.Optimizer, criterion: nn.Module, train_dataset: Dataset, test_dataset: Dataset):
+def train(model: nn.Module, train_dataset: Dataset, test_dataset: Dataset, val_dataset: Dataset, n_epochs: int, batch_size: int, optimizer: optim.Optimizer, criterion: nn.Module, scheduler: object = None):
     train_losses = []
     test_losses = []
     train_accuracies = []
@@ -127,19 +149,26 @@ def train(model: nn.Module, n_epochs: int, batch_size: int, optimizer: optim.Opt
     model_to_save = {}
 
     train_gen = DataLoader(train_dataset, batch_size, shuffle=True)
+    val_gen = DataLoader(val_dataset, batch_size, shuffle=False)
     test_gen = DataLoader(test_dataset, batch_size, shuffle=False)
+
+    if torch.cuda.is_available():
+        model = model.cuda()
+        criterion = criterion.cuda()
 
     for epoch_number in range(n_epochs):
         train_loss_sum = 0
         print(f"\n------ EPOCH {epoch_number} ------\n")
 
-        train_loss, train_accuracy = train_epoch(model, optimizer, criterion, train_gen)
-        test_loss, test_accuracy = test_epoch(model, criterion, test_gen)
+        train_loss, train_accuracy = train_epoch(model, train_gen, val_gen, optimizer, criterion, scheduler=scheduler)
+        test_loss, test_accuracy = test_epoch(model, test_gen, criterion)
         
         train_losses.append(train_loss)
         test_losses.append(test_loss)
         train_accuracies.append(train_accuracy)
         test_accuracies.append(test_accuracy)
+
+        test_realworld_images(model)
 
         if (model_to_save.get('test_accuracy', 0) < test_accuracy):
             model_to_save = {
@@ -159,26 +188,28 @@ def train(model: nn.Module, n_epochs: int, batch_size: int, optimizer: optim.Opt
 
 def run():
     # Parameters
-    n_epochs = 30
+    n_epochs = 15
     batch_size = 128
-    lr = 0.0003
+    lr = 0.001
 
     # Loading dataset
     train_dataset = SignLanguageDataset("train")
+    train_dataset, val_dataset = split_dataset(train_dataset, 0.9)
     test_dataset = SignLanguageDataset("test")
 
     # TODO create Dataloader to load in batches
 
     # Creating model
-    model = Net(26)
-    # model = GoogleNet(train_y.max() + 1)
+    model = Net(25)
+    # model = GoogleNet(25)
 
     # Creating learning modules
     model_parameters = filter(lambda x: x.requires_grad, model.parameters())
     optimizer = optim.Adam(model_parameters, lr = lr)
     criterion = nn.CrossEntropyLoss()
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.5, patience=2, min_lr=0.00001, verbose=True)
 
-    train(model, n_epochs, batch_size, optimizer, criterion, train_dataset, test_dataset)
+    train(model, train_dataset, val_dataset, test_dataset, n_epochs, batch_size, optimizer, criterion, scheduler=scheduler)
 
 
 
