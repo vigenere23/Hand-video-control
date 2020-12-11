0a63fb1547924825da7b19fc697f65493c027073
GASTP33 <gabriel.st-pierre.6@ulaval.ca>
2020-12-11 16:54:32 -0500

    new models

--- dataset.py
+++ dataset.py
@@ -46,7 +46,7 @@ class TrainSignLanguageDataset():
 
         self.transform = nn.Sequential(
             # TODO normalize?
-            # transforms.RandomHorizontalFlip(),
+            transforms.RandomHorizontalFlip(),
             transforms.RandomAffine(10, translate=(0.2, 0.2), scale=(0.8, 1.2), fillcolor=255),
             # transforms.GaussianBlur(3)
         )
