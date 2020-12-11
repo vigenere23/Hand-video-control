b2def36ef90bdbd2bd14c3e8356c474021cf6a78
Damien LaRocque <phicoltan@gmail.com>
2020-12-10 23:15:04 -0500

    Add instance mode for vlc player

--- vlc_media_player.py
+++ vlc_media_player.py
@@ -7,17 +7,32 @@ class VLCMediaPlayer:
     Play a video with VLC, from a YouTube url
     """
 
-    def __init__(self, url="https://www.youtube.com/watch?v=xtp4msMYi9s"):
+    def __init__(self, url="https://www.youtube.com/watch?v=xtp4msMYi9s", mode=None):
         self.url = url
         video = pafy.new(self.url).getbest()
-        self.media_player = vlc.MediaPlayer(video.url)
+        if str(mode).lower() == "terminal":
+            self._player = vlc.MediaPlayer(video.url)
+        else:
+            instance = vlc.Instance("--loop")
+            self._player = instance.media_player_new()
+            media = instance.media_new(video.url)
+            media.get_mrl()
+            self._player.set_media(media)
+
 
     def play(self):
-        self.media_player.play()
+        self._player.play()
+
 
 
 if __name__ == "__main__":
+    # media_player = VLCMediaPlayer(mode="terminal")
+    # try:
+    #     media_player.play()
+    #     input("Quit ?")
+    # except:
+    #     print("Excepted")
+    #     # media_player.stop()
     media_player = VLCMediaPlayer()
     media_player.play()
-    while True:
-        pass
+    input("Quit ?")
